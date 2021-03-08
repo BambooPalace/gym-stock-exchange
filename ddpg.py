57899ec3b3a436c4ac02b68b6ec7b4852f1c6e20
Claire Gong <aprilgng@gmail.com>
2021-03-08 14:57:56 +0800

    add

--- ddpg.py
+++ ddpg.py
@@ -2,9 +2,10 @@ import gym
 import gym_exchange
 from collections import Counter, defaultdict
 import time
-from stable_baselines.common.policies import MlpPolicy
+# from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import A2C
+from stable_baselines import DDPG
+from stable_baselines.ddpg.policies import MlpPolicy
 
 
 if __name__ == '__main__':
@@ -13,19 +14,19 @@ if __name__ == '__main__':
     env = gym.make('game-stock-exchange-continuous-v0')
     env = DummyVecEnv([lambda: env])
 
-    model = A2C(MlpPolicy, env, ent_coef=0.1, verbose=1)
-    model = A2C.load("a2c_gym_exchange_continuous", env=env)
+    model = DDPG(MlpPolicy, env, verbose=1)
+    # model = A2C.load("a2c_gym_exchange_continuous", env=env)
     model.learning_rate = 1e-7
 
     # Train the agent
-    # model.learn(total_timesteps=100000) # default=1000000
+    model.learn(total_timesteps=100000) # default=1000000
 
     # Save the agent
-    # model.save("a2c_gym_exchange_continuous")
+    model.save("ddpg_gym_exchange_continuous")
     # del model  # delete trained model to demonstrate loading
 
     # Load the trained agent
-    # model = A2C.load("a2c_gym_exchange_continuous")
+    model = DDPG.load("ddpg_gym_exchange_continuous", env=env)
 
     # Enjoy trained agent
     obs = env.reset()
